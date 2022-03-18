e882a188e1ac6e24489484fcc3a52cf57a597343
TarleyRodrigues <tarley.divino@gmail.com>
2022-03-18 09:40:37 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,63 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		List<Product> list = new ArrayList<>();
+
+
+		File caminhoArquivo = new File("D:\\ProgramasJava\\out\\summary.txt");
+
+		System.out.print("Quantidade de produtos: ");
+		int num = sc.nextInt();
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(caminhoArquivo, true))) {
+			for (int i = 1; i <= num; i++) {
+				System.out.println("Product #" + i + " Data:");
+				System.out.print("Nome: ");
+				sc.nextLine();
+				String name = sc.nextLine();
+				System.out.print("Preço: ");
+				Double price = sc.nextDouble();
+				System.out.print("Quantidade: ");
+				int quantity = sc.nextInt();
+				list.add(new Product(name, price, quantity));
+			}
+
+			String productVect[] = new String[num];
+			int cont = 0;
+			for (Product p : list) {
+				productVect[cont] = p.toString();
+				cont++;
+				}
+			
+			boolean sucess = new File(caminhoArquivo + "\\out").mkdir();
+			System.out.println("Diretório criado com sucesso: " + sucess);
+
+			for (String line : productVect) {
+				bw.write(line);
+				bw.newLine();
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+
+			sc.close();
+
+		}
+
+	}}
---
+++ src/entities/Product.java
@@ -0,0 +1,67 @@
+package entities;
+
+public class Product {
+	
+	private String name;
+	private Double price;
+	private Integer quantity;
+	private Double total;
+	
+	public Product() {
+	}
+
+	public Product(String name, Double price, Integer quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+		this.total = totalValue(price, quantity);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+
+	public Double totalValue(Double price, int quantity) {
+		return quantity * price;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		
+		builder.append(name);
+		builder.append(", ");
+		builder.append(String.format("%.2f", total));
+		return builder.toString();
+	}
+	
+}
